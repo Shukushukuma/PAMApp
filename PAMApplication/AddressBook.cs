f9d5d8cde1d35bf575977243fa5e8df415089935
Shukushukuma <65419296+Shukushukuma@users.noreply.github.com>
2020-05-21 14:44:22 +0200

    Add files via upload

---
+++ PAMApplication/AddressBook.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+// public class List<AddressBook>
+// {
+//     //public List<Person> PersonID;
+//     public Address Address;
+
+//     // public void setAddress(Person pID, Address pAddress)
+//     // {
+//     //     PersonID=pID;
+//     //     Address=pAddress;
+
+//     // }
+// }
